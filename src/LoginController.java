0be31c0a7263cf54bd621da3d867d50694992994
calebcheptumo <ckibusia@gmail.com>
2023-11-15 12:23:46 +0300

    boilerplate

---
+++ src/LoginController.java
@@ -0,0 +1,32 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+public class LoginController {
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private void handleLogin(ActionEvent event) {
+        // Your existing code here...
+    }
+
+    private void openMainDashboard() {
+        // Your existing code here...
+    }
+
+    private void showAlert(String title, String content) {
+        // Your existing code here...
+    }
+}
